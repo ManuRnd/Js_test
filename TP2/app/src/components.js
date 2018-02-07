73745ee9625a9b71b52a0d99f61a74b067ec2533
manuel <renaudineau.manuel@orange.fr>
2018-02-07 17:47:45 +0100

    des tests fonctionnent

--- TP2/app/src/components.js
+++ TP2/app/src/components.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
 
 export class InputArea extends Component {
+    constructor(props){
+        super(props);
+        this.state={text:''};
+        this.setTexte=this.setTexte.bind(this);
+        this.handleClick=this.handleClick.bind(this);
+
+    }
+
+    setTexte(ev){
+        this.state.text=ev.target.value;
+    }
+
+    handleClick(){
+        this.props.onSubmit(this.state.text);
+    }
+
     render() {
-        return <input/>
+        return (
+            <div>
+            <input value={this.state.text} onChange={this.setTexte}/>
+            <button onClick={this.handleClick}>Add</button>
+            </div>
+        )
     }
 }
 
@@ -15,13 +36,19 @@ export class TodoListContainer extends Component{
     constructor(){
         super();
         this.state={todos:[]};
+        this.addItem=this.addItem.bind(this);
+    }
+    addItem(name){
+        this.state.todos.push(name);
     }
     render(){
         return (
             <div>
-            <InputArea/>
+            <InputArea onSubmit={this.addItem}/>
             <TodoList/>
         </div>
         )
     }
+
 }
+
--- TP2/app/test/test.spec.js
+++ TP2/app/test/test.spec.js
@@ -3,6 +3,7 @@ import { expect,assert } from 'chai';
 import { shallow, mount, render, configure } from 'enzyme';
 import { TodoListContainer, InputArea, TodoList } from '../src/components';
 import Adapter from 'enzyme-adapter-react-16';
+import { spy } from 'sinon';
 
 configure({ adapter: new Adapter() });
 
@@ -18,5 +19,53 @@ describe('TodoListContainer', () => {
     it('state test',()=>{
         const container= shallow(<TodoListContainer/>);
         expect(container.state('todos')).to.eql([]);
-    })
+    });
+    it('addItem',()=>{
+        const container= shallow(<TodoListContainer/>);
+        container.instance().addItem('TOTO');
+        expect(container.state('todos')).to.eql(['TOTO']);
+    });
+
+    it('submit',()=>{
+        const input=shallow(<InputArea/>);
+        const add=input.instance().addItem;
+        expect(input.prop("onSubmit")).to.eql(add);
+    });
+
+    it('binding',()=>{
+        const container=shallow(<TodoListContainer/>);
+        const onsub=container.find('InputArea').prop('onSubmit');
+        onsub('Bonjour');
+        expect(container.state('todos')).to.eql(['Bonjour']);
+    });
+});
+
+describe('InputArea', () => {
+    it('should render InputArea and TodoList', () => {
+        const wrapper = shallow(<InputArea/>);
+        expect(wrapper.containsAllMatchingElements([
+            <input/>,
+            <button>Add</button>
+        ])).to.equal(true);
+    });
+    it('form', () => {
+        const wrapper = shallow(<InputArea/>);
+        const input = wrapper.find('input');
+        input.simulate('change',{target : {value : 'text'}});
+
+        expect(wrapper.state('text')).to.eql('text');
+
+    });
+
+    it('Validate Submit', () => {
+
+        const addItemSpy = spy();
+        const wrapper = shallow(<InputArea onSubmit={addItemSpy}/>);
+        wrapper.setState({text:'text'});
+        const button= wrapper.find('button');
+
+        button.simulate('click');
+        expect(addItemSpy.calledOnce).to.eql(true);
+        expect(addItemSpy.calledWith('text')).to.equal(true);
+    });
 });
